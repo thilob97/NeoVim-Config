092f506c7fb6f0b93ab38525f3d3a811947491e0
Thilo Barth <barth-thilo@proton.me>
2024-05-26 00:20:38 +0200

    add theme and leetcode

--- init.lua
+++ init.lua
@@ -869,6 +869,23 @@ require("lazy").setup({
 			--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 		end,
 	},
+	{ -- You can easily change to a different colorscheme.
+		-- Change the name of the colorscheme plugin below, and then
+		-- change the command in the config to whatever the name of that colorscheme is.
+		--
+		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+		"folke/tokyonight.nvim",
+		priority = 1000, -- Make sure to load this before all the other start plugins.
+		init = function()
+			-- Load the colorscheme here.
+			-- Like many other themes, this one has different styles, and you could load
+			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+			vim.cmd.colorscheme("tokyonight-moon")
+
+			-- You can configure highlights by doing something like:
+			vim.cmd.hi("Comment gui=none")
+		end,
+	},
 	{
 		{
 			"nvim-neo-tree/neo-tree.nvim",
@@ -898,6 +915,23 @@ require("lazy").setup({
 		},
 		{
 			{ "github/copilot.vim" },
+		},
+		{
+			"kawre/leetcode.nvim",
+			build = ":TSUpdate html",
+			dependencies = {
+				"nvim-telescope/telescope.nvim",
+				"nvim-lua/plenary.nvim", -- required by telescope
+				"MunifTanjim/nui.nvim",
+
+				-- optional
+				"nvim-treesitter/nvim-treesitter",
+				"rcarriga/nvim-notify",
+				"nvim-tree/nvim-web-devicons",
+			},
+			opts = {
+				-- configuration goes here
+			},
 		},
 	},
 
